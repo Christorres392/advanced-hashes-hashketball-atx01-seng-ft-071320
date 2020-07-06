7be541545655b778c058506c6323069bc547d7c9
Christopher Torres <christorres09@gmail.com>
2020-07-06 10:48:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -227,6 +227,7 @@ def player_stats (player_name)
         if player_stats[:shoe] > big_shoe
           big_shoe = player_stats[:shoe]
           rebounds = player_stats[:rebounds]
+        end
       end
     end
     rebounds
