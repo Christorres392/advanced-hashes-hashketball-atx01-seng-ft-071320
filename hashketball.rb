0008de19f866cfa1cb3af943c5eb256e24231ae0
Christopher Torres <christorres09@gmail.com>
2020-07-06 10:39:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -210,3 +210,16 @@ def player_stats (player_name)
       end
     end
   end
+
+  #* Build a method, `big_shoe_rebounds`, that will return the number of rebounds
+  #  associated with the player that has the largest shoe size. Break this one down
+  #  into steps:
+
+  #  * First, find the player with the largest shoe size
+  #  * Then, return that player's number of rebounds
+  #  * Remember to think about return values here.
+
+  def big_shoe_rebounds 
+    rebounds = nil
+    big_shoe = nil
+    
