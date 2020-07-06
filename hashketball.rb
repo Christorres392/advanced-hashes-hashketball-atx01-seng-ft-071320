8e7e07e88c70524d72dd2c8a999fbedb967f301b
Christopher Torres <christorres09@gmail.com>
2020-07-06 10:44:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -223,8 +223,7 @@ def player_stats (player_name)
     rebounds = nil
     big_shoe = nil
     game_hash.each do |loc, data|
-      binding.pry
-      data.each do |player_stats|
+      data[:player].each do |player_stats|
       binding.pry
       end
     end
